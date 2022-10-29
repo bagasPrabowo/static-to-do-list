3fdc89f2bdbf9933d28e91f789186615c82ee89d
bagas.prbowo@gmail.com <bagas.prbowo@gmail.com>
2022-10-29 10:12:23 +0700

    display-list

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function getList(){
-    console.log('Hi From JS');
+function addList(){
+    let listItem = document.getElementById('list-item');
+    let newList = `<li>${getText()}</li>`;
+    listItem.insertAdjacentHTML('beforeend', newList);
+}
+
+function getText(){
+    return document.querySelector('input').value;
 }
