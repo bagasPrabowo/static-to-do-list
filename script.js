d179daf3c1decfdb9d43d1bd5fe28d46c1a92ed3
bagas.prbowo@gmail.com <bagas.prbowo@gmail.com>
2022-10-31 22:01:14 +0700

    edit and delete function

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
+let idx = 0;
 function addList(){
     let listItem = document.getElementById('list-item');
-    let newList = `<li>${getText()}</li>`;
+    let newList = 
+            `<div>
+                <li id="list${idx}">${getText()}</li>
+                <button class="btn" type="submit" onclick="editList(${idx})">Edit</button>
+                <button class="btn" type="submit" onclick="deleteList(this)">Delete</button>
+            </div>`;
     listItem.insertAdjacentHTML('beforeend', newList);
+    idx++;
 }
 
 function getText(){
     return document.querySelector('input').value;
 }
 
-function deleteList(){
-    return document.writeln('deleted')
+function deleteList(list){
+    list.parentNode.remove();
 }
 
-function editList(){
-    return document.writeln('edited');
+function editList(idx){
+    let listIdx = document.getElementById(`list${idx}`);
+    listIdx.innerHTML = getText();
 }
